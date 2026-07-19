22c9225833ab1a38568a3037160fc1bc94dd6685
Mohammed Khafagy <miidoo752000@gmail.com>
2026-07-19 12:00:53 +0300

    Add files via upload

---
+++ scripts/bronze/load_to_bronze.sql
@@ -0,0 +1,83 @@
+EXEC BRONZE.load_bronze;
+
+CREATE OR ALTER PROCEDURE bronze.load_bronze As
+BEGIN
+PRINT '=================================================================='
+PRINT 'LOADING BRONZE LAYER '
+PRINT '=================================================================='
+
+PRINT '------------------------------------------------------------------'
+PRINT 'LOADING CRM TABLES'
+PRINT '------------------------------------------------------------------'
+
+PRINT '>> TRUNCATIN TABLE: bronze.crm_cust_info'
+TRUNCATE TABLE bronze.crm_cust_info
+
+PRINT '>> INSERING DATA TABLE: bronze.crm_cust_info'
+BULK INSERT bronze.crm_cust_info
+FROM 'D:\DW_project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+WITH(
+FIRSTROW=2,
+FIELDTERMINATOR=',',
+TABLOCK
+);
+
+PRINT '>> TRUNCATIN TABLE: bronze.crm_prd_info'
+TRUNCATE TABLE bronze.crm_prd_info
+PRINT '>> INSERING DATA TABLE: bronze.crm_prd_info'
+BULK INSERT bronze.crm_prd_info
+FROM 'D:\DW_project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+WITH(
+FIRSTROW=2,
+FIELDTERMINATOR=',',
+TABLOCK
+);
+
+PRINT '>> TRUNCATIN TABLE: crm_sales_details'
+TRUNCATE TABLE bronze.crm_sales_details
+PRINT '>> INSERING DATA TABLE: bronze.crm_sales_details'
+BULK INSERT bronze.crm_sales_details
+FROM 'D:\DW_project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+WITH(
+FIRSTROW=2,
+FIELDTERMINATOR=',',
+TABLOCK
+);
+
+PRINT '------------------------------------------------------------------'
+PRINT 'LOADING ERP TABLES'
+PRINT '------------------------------------------------------------------'
+
+PRINT '>> TRUNCATIN TABLE: bronze.erp_cust_az12'
+TRUNCATE TABLE bronze.erp_cust_az12
+PRINT '>> INSERING DATA TABLE: bronze.erp_cust_az12'
+BULK INSERT bronze.erp_cust_az12
+FROM 'D:\DW_project\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
+WITH(
+FIRSTROW=2,
+FIELDTERMINATOR=',',
+TABLOCK
+);
+
+PRINT '>> TRUNCATIN TABLE: bronze.erp_loc_a101'
+TRUNCATE TABLE bronze.erp_loc_a101
+PRINT '>> INSERING DATA TABLE: bronze.erp_loc_a101'
+BULK INSERT bronze.erp_loc_a101
+FROM 'D:\DW_project\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+WITH(
+FIRSTROW=2,
+FIELDTERMINATOR=',',
+TABLOCK
+);
+
+PRINT '>> TRUNCATIN TABLE: bronze.erp_px_cat_g1v2'
+TRUNCATE TABLE bronze.erp_px_cat_g1v2
+PRINT '>> INSERING DATA TABLE: bronze.erp_px_cat_g1v2'
+BULK INSERT bronze.erp_px_cat_g1v2
+FROM 'D:\DW_project\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+WITH(
+FIRSTROW=2,
+FIELDTERMINATOR=',',
+TABLOCK
+);
+END
